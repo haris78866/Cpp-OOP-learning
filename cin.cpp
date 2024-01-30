ee6e41ce8d8a7de125661beba1cf2bca0613e51d
Haris <144903716+haris78866@users.noreply.github.com>
2024-01-30 13:47:51 +0500

    Add files via upload

---
+++ cin.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+int x,y;
+cout<<"Enter the value of x: "<<endl;
+cin>>x;
+
+cout<<"Enter the value of y: "<<endl;
+cin>>y;
+
+
+cout<<"Value of y: "<<y<<" and x : "<<x<<endl;
+
+return 0;
+} 
