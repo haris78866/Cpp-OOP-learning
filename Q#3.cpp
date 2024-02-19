c7f1934e30b2c85a304d28bdfbd2dc536cd1782d
Haris <144903716+haris78866@users.noreply.github.com>
2024-02-19 20:01:04 +0500

    Add files via upload

---
+++ Q#3.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+
+using namespace std;
+
+int main()
+{
+int rows;
+cout<<"Enter the num of rows :"<<endl;
+cin>>size;
+
+]
+int n;
+for(int i=0;i<size;i++){
+    cout<<"Enter how many elemnet should have for each row :"<<endl; 
+     cin>>n;          // how many elemnets should have in each row its means how many coloumn  for the coloumn i declared   variable n
+   
+    for(int j=0;j<n;j++){
+int arr[size][n];
+      cin>>arr[i][[j];
+      }
+      
+for(int i=0;i<size;i++){
+    for(int j=0;j<n;j++){
+       cout<<arr[i][j];
+      }
+     }        
+
+return 0;
+}
