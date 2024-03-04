629273515ffcf192fffa173a59fe812c5a49b32a
Haris <144903716+haris78866@users.noreply.github.com>
2024-03-04 16:22:02 +0500

    Add files via upload

--- Dynamic-Memory.cpp
+++ Dynamic-Memory.cpp
@@ -3,32 +3,109 @@
 #include<iostream>
 
 using namespace std;
-
+void taking();
 int main()
 {
 
-int *ptr =new int(5);
-
-cout<<" value : "<<*ptr<<endl;
-cout<<" address : "<<ptr<<endl;
-
-delete ptr;
+taking();
 
 
-int *array = new int[4];
-
-array[0]=1;
-array[1]=3;
-array[2]=2;
-array[3]=5;
-
- for(int i=0;i<4;i++)
-    { 
-      cout<<"["<<array[i]<<"]"<<endl;
-      }
-      
-      
- delete array;
- 
 return 0;
 }
+
+// this is dynamically  allocated  for 1D array
+
+ 
+/*
+void Taking_value()
+{
+int size;
+
+cout<<"Enter the size of an array :"<<endl;
+cin>>size;
+
+
+int *array;
+
+array=new int(size);
+
+// enter the elment in the array 
+
+int i;
+
+cout<<"enter the elments in the array"<<endl;
+for(i=0;i<size;i++){
+   cin>>array[i];
+   }
+   
+   for(i=0;i<size;i++){
+   cout<<"index : "<<" "<<i<<array[i]<<endl;
+   
+   }
+   
+   delete array;
+   
+
+  
+}
+*/
+
+// this is dyanmically alloacated for 2D arrays
+void taking()
+{
+int size;
+
+cout<<"Enter the size of an array : "<<endl;
+cin>>size;
+
+
+int **array;
+array=new int*[2];
+
+int i,j;
+
+for(i=0;i<size;i++)
+  {
+  array[i]=new int[2];
+  }
+  
+  
+cout<<"Taking elemnet in the array :"<<endl;
+
+for(i=0;i<size;i++)
+   {
+   for(j=0;j<size;j++){
+   cin>>array[i][j];
+   }
+  }
+
+
+for(i=0;i<size;i++)
+   {
+   for(j=0;j<size;j++){
+   cout<<"element : "<<i<<" "<<j<<array[i][j]<<endl;
+   }
+  }
+
+delete []array;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
