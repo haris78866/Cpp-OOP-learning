e600be06a999ee9545369775890845cfd5403f0a
Haris <144903716+haris78866@users.noreply.github.com>
2024-03-14 00:13:00 +0500

    Add files via upload

--- static.cpp
+++ static.cpp
@@ -3,117 +3,148 @@
 using namespace std;
 
 
-class info {
-private:
-string *name;
-string *job;
-int *age;
+ // instance variable  (that uses operater with the class) 
+ /*
+class  animal {
 
-public:
-// default constructer 
-info(){
-name=new string;
-job=new string;
-age=new int;
-}
+public: 
+string species;
 
-// parameterized constructer 
-info(string name, string job, int age){
-this->name=new string(name);
-this->job=new string(job);
-this->age=new int(age);
-
-cout<<"Constructer is executive :"<<endl;
-cout<<"Name :  "<<*this->name<<endl;
-cout<<"Job  :  "<<*this->job<<endl;
-cout<<"Age  :  "<<*this->age<<endl;
-}
-
-// destructer 
-
-~info(){
-cout<<"Destructer is executive :"<<endl;
-cout<<"Name :  "<<*name<<endl;
-cout<<"Job  :  "<<*job<<endl;
-cout<<"Age  :  "<<*age<<endl;
-delete name;
-delete job;
-delete age;
-}
-
-// getters 
-
-string getname(){
-return *name;
-}
-
-string getjob(){
-return *job;
-}
-
-int getage(){
-return *age;
-}
-
-
-// setters 
-
-
-void setname(string name){
-*this->name=name;
-}
-
-
-void setjob(string job){
-*this->job=job;
-}
-
-
-void setage(int age){
-*this->age=age;
-}
-
-
-void display(){
-cout<<"Information :"<<endl;
-cout<<"Name :  "<<*name<<endl;
-cout<<"Job  :  "<<*job<<endl;
-cout<<"Age  :  "<<*age<<endl;
+// constructer
+animal (string species){
+this->species=species;
 }
 
 };
 
-
-
 int main(){
 
-int no;
-cout<<"Enter the total person :"<<endl;
-cin>>no;
-
-info p[no];
+animal *lion=new animal("sherry");
+animal *tiger=new animal("tarzon");
 
 
-string name,job;
-int age;
-
-for(int i=0;i<no;i++){
-cout<<"Enter the name : "<<i+1<<endl;
-cin>>name;
-p[i].setname(name);
-
-cout<<"Enter the Job title : "<<i+1<<endl;
-cin>>job;
-p[i].setjob(job);
-
-cout<<"Enter your age  : "<<i+1<<endl;
-cin>>age;
-p[i].setage(age);
-}
-
-for(int i=0;i<no;i++){
-p[i].display();
-}
+cout<<"lion  : "<<lion->species<<endl;
+cout<<"Tiger : "<<tiger->species<<endl;
 
 return 0;
 }
+
+*/
+
+// The concept of static variable 
+
+/*
+class  animal {
+
+public: 
+string species;
+static  int total;
+
+const string planet ="Earth";
+
+// constructer
+animal (string species){
+this->species=species;
+total++;
+}
+
+
+~animal(){
+total--;
+}
+
+};
+
+// we can not use this  inside the class (give an error)   
+// if we use const variable instead  static then we can use inside the class 
+int animal :: total=0;
+
+int main(){
+
+animal *lion=new animal("sherry");
+animal *tiger=new animal("tarzon");
+
+
+cout<<"lion  : "<<lion->species<<endl;
+cout<<"Tiger : "<<tiger->species<<endl;
+cout<<"Total : "<<animal :: total<<endl;
+
+delete lion;
+delete tiger;
+
+cout<<"Total : "<<animal :: total<<endl;
+
+
+return 0;
+}
+
+*/
+
+
+// static member function  
+
+
+class info {
+
+private:
+int length;
+static int total;
+
+public:
+
+// constructer
+info (int length){
+this->length=length;
+total++;
+}
+
+~info(){
+cout<<"Destructer is exceuting : "<<endl;
+total--;
+}
+
+int area(){
+return length*length;
+}
+
+int width(){
+return length+length;
+}
+
+
+static int get_total(){
+return total;
+}
+
+};
+
+int info :: total =0;
+
+int main(){
+// square 1
+info square1(5);
+
+cout<<"Area  : "<<square1.area()<<endl;
+cout<<"Width : "<<square1.width()<<endl;
+
+
+// we have to ways to call static member fun 
+cout<<"Total squares : "<<square1.get_total()<<endl;
+cout<<"Total squares : "<<info :: get_total()<<endl;
+
+// now for square 2 
+info square2(10);
+
+cout<<"Area  : "<<square2.area()<<endl;
+cout<<"Width : "<<square2.width()<<endl;
+
+
+// we have to ways to call static member fun 
+cout<<"Total squares : "<<square2.get_total()<<endl;
+cout<<"Total squares : "<<info :: get_total()<<endl;
+
+
+
+return 0;
+}
+
