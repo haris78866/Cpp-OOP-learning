d8d8b620dc10a65360e66e09d0eff255d0daaba3
Haris <144903716+haris78866@users.noreply.github.com>
2024-06-03 00:12:15 +0500

    Add files via upload

--- pra.cpp
+++ pra.cpp
@@ -1,49 +1,124 @@
 #include<iostream>
 #include<string>
-
 using namespace std;
 
 
-class Name 
-{
-public:
- string name;
- 
- void total()
- {
- cout<<"My age is "<<my_age()<<endl;
- }
- 
- void print()
- {
- cout<<"My name is : "<<name<<endl;
- }
-
-private:
-int age=12;
-
-int my_age()
-{
-return age;
-} 
-};
+void tak(int **mat1, int **mat2);
+void  show(int **mat1, int **mat2);
+void  addition(int **mat1, int **mat2,int **add);
 
 
+int main(){
+int **mat1;
+int **mat2;
+int **add;
+
+mat1=new int *[3];
+mat2=new int *[3];
+add=new int *[3];
+
+for(int i=0;i<3;i++){
+mat1[i]=new int[3];
+mat2[i]=new int[3];
+add[i]=new int [3];
+}
 
 
+tak(mat1,mat2);
+show(mat1,mat2);
+addition(mat1,mat2,add);
 
 
-
-int main()
-{
- Name myname1;
- myname1.name="Haris";
- 
- 
- myname1.print();
- myname1.total();
- 
-
+for(int i=0;i<3;i++){
+delete mat1[i];
+delete mat2[i];
+delete add[i];
+}
+delete mat1;
+delete mat2;
+delete add;
 
 return 0;
 }
+
+
+
+void  tak(int **mat1, int **mat2){
+
+
+cout<<"Enter elements int the 1st matrix : "<<endl;
+for(int i=0;i<3;i++){
+  for(int j=0;j<3;j++){
+  cin>>mat1[i][j];
+  }
+}
+
+
+cout<<"Enter elements int the 1st matrix : "<<endl;
+for(int i=0;i<3;i++){
+  for(int j=0;j<3;j++){
+      cin>>mat2[i][j];
+  }
+}
+
+
+}
+
+
+
+void  show(int **mat1, int **mat2){
+
+
+cout<<"Elements int the 1st matrix : "<<endl;
+for(int i=0;i<3;i++){
+  for(int j=0;j<3;j++){
+  cout<<mat1[i][j]<<" ";
+  }
+  cout<<endl;
+}
+
+
+cout<<"Elements int the 1st matrix : "<<endl;
+for(int i=0;i<3;i++){
+  for(int j=0;j<3;j++){
+      cout<<mat2[i][j]<<"  ";
+  }
+  cout<<endl;
+}
+
+
+}
+
+
+
+void  addition(int **mat1, int **mat2,int **add){
+
+
+
+for(int i=0;i<3;i++){
+  for(int j=0;j<3;j++){
+  add[i][j]=mat1[i][j]+mat2[i][j];
+  }
+  cout<<endl;
+}
+
+
+cout<<"Addition of  1st matrix  and 2nd matrix : "<<endl;
+for(int i=0;i<3;i++){
+  for(int j=0;j<3;j++){
+      cout<<add[i][j]<<"  ";
+  }
+  cout<<endl;
+}
+
+
+}
+
+
+
+
+
+
+
+
+
