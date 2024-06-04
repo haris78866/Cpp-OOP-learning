242a5eac0f4e879897f54550e9787b4e657b4652
Haris <144903716+haris78866@users.noreply.github.com>
2024-06-05 00:03:05 +0500

    Add files via upload

--- Q1.cpp
+++ Q1.cpp
@@ -2,179 +2,108 @@
 #include<string>
 using namespace std;
 
-class dataType {
+
+class oriion_space_station_23_0573 {
 private:
-string day;
+string choice;
+
+
 public:
 
-// default costructer
- dataType(){
- day="Sun";
+oriion_space_station_23_0573(string choice) : choice(choice){
 }
 
-// parameterized constructer
-dataType(string day){
-this->day=day;
+
+
+string *tools;
+string *supplies;
+string *artifacts;
+string *repair;
+
+int n;
+
+void add_tools(string *tools,string *supplies,string *articrafts,string *repair){
+
+cout<<"Enter the no of items you wanna add :"<<endl;
+cin>>n;
+tools=new string[n];
+supplies=new string[n];
+artifacts=new string[n];
+repair=new string[n];
+
+for(int i=0;i<n;i++){
+cout<<"Enter the tools "<<endl;
+cin>>tools[i];
+cout<<"Enter the supplies :"<<endl;
+cin>>supplies[i];
+cout<<"Enter the artifacts "<<endl;
+cin>>artifacts[i];
+cout<<"Enter the repair "<<endl;
+cin>>repair[i];
+}
 }
 
-// copy constructer
+void start(string choice){
+if(choice == "Y" || "y"){
+do{
+cout<<"1)You wanna add to inventory :"<<endl;
+cout<<"2)select category (tools , supplies, articrafts , repair material ):"<<endl;
+cout<<"End game : "<<endl;
+int c;
+cout<<"Enter your choice "<<endl;
+cin>>c;
+int no;
+switch(choice){
 
-dataType(const dataType & other){
-day=other.day;
-}
-
-// setters
-
-
-void setday(string day){
-this->day=day;
-}
-
-// getter 
-
-string getday(){
-return day;
-}
-
-// print the day
-void printday() {
-cout<<"Day : "<<day<<endl;
-}
-
-// next day 
-string getnextday(){
- if(day=="Sun")
-        return "Mon";
- else if(day=="Mon")
-        return "Tue";
- else if(day=="Tue")
-        return "Wed";            
- else if(day=="Wed")
-        return "Thu";      
- else if(day=="Thu")
-        return "Fri";
- else if(day=="Fri")
-        return "Sat";
- else if(day=="Sat")
-        return "sun";
- else 
-      cout<<"Invalid Day :";
-}
-
-// previous day 
-
-string getpreviousDay(){
-  if(day=="Sun")
-        return "Sat";
- else if(day=="Sat")
-        return "Fri";
- else if(day=="Fri")
-        return "Thu";            
- else if(day=="Thu")
-        return "Wed";      
- else if(day=="Wed")
-        return "Tue";
- else if(day=="Tue")
-        return "Mon";
- else if(day=="Mon")
-        return "Sun";
- else 
-      cout<<"Invalid Day :"<<endl;
-}
-
-string getdayafter(int numdays){
-int index=0;
-if(day=="Sun")
-      index =0 ;
- else if(day=="Mon")
-        index =1; 
- else if(day=="Tue")
-       index =2;           
- else if(day=="Wed")
-        index =3;       
- else if(day=="Thu")
-        index =4; 
- else if(day=="Fri")
-        index =5;
- else if(day=="Sat")
-        index =6;
+case 1 :
+       add_tools(tools,supplies,articrafts,repair);
+       break;
+case 2 :
  
- int newIndex =(index +numdays)%7;
- string days[7]={ "Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
- return days[newIndex];
+       cout<<"Enter the num "<<endl;
+       cin>>no;
+      if(no==1){
+       for(int i=0;i<n;i++){
+      cout<<"Avalible Tool :"<<tools[i]<<endl;
+      }
+      cout<<"Enter  the index no which do you wnat respect to 0 "<<endl;
+      cin>>ind1;
+      
+      }
+      else if(no==2){
+    for(int i=0;i<n;i++){
+      cout<<"Avalible suuplies :"<<supplies[i]<<endl;
+      }
+      }
+      else if(no==3){
+    for(int i=0;i<n;i++){
+      cout<<"Avalible Articfacts :"<<artifacts[i]<<endl;
+      }
+      }
+      else if(no==4){
+    for(int i=0;i<n;i++){
+      cout<<"Avalible repair material  :"<<repair[i]<<endl;
+      }
+      }
+      else {
+   cout<<"no more choice avalible :"<<endl;
+     }
+       break ;
+      
+      
+      
 }
 
 
-// calling the destructer
-~dataType(){
-}
 
 };
 
 
 int main(){
-// taking object dynamicallly
-dataType *dayObj=new dataType();
-char choice;
-string newDay;
-int numday;
 
-do{
-cout<<"a) Set Day :               "<<endl;
-cout<<"b) Print the Day           "<<endl;
-cout<<"c) Return the Day          "<<endl;
-cout<<"d) Return the next Day     "<<endl;
-cout<<"e) Return the previous day "<<endl;
-cout<<"f) Calculate and return the day by adding days to the current day "<<endl;
-cout<<"g) EXIT              :     "<<endl;
-cout<<" Enter your choice :       "<<endl;
-cin>>choice;
 
-switch(choice){
-      case 'a':
-            cout<<"Enter a day :"<<endl;
-            cin>>newDay;
-            dayObj->setday(newDay);
-      break;
-      
-      case 'b':
-            dayObj->printday();
-      break;
-      
-      case 'c':
-            cout<<"Current Day :"<<dayObj->getday()<<endl;
-      break;
-      
-      case 'd':
-            cout<<"Next Day :"<<dayObj->getnextday()<<endl;
-      break;
-      
-      case 'e':
-             cout<<"Next Day :"<<dayObj->getpreviousDay()<<endl;
-      break;
-      
-      case 'f':
-            cout<<"Enter the num of days to  Add :"<<endl;
-            cin>>numday;
-            cout<<"New day after adding :"<<dayObj->getdayafter(numday)<<endl;
-      break;
-      
-      case 'g':
-       cout<<"EXIT :"<<endl;
-      break;
-      
-default:
-      cout<<"Invlaid choice :"<<endl;
-      break;
-     }            
-}while(choice!='g');
 
-// deallocating  dayObj
-delete dayObj;
 
-exit(0);
 return 0;
 }
-
-
 
